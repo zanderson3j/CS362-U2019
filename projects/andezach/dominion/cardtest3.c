8de1a96037e408d5ab3d37923350c097ff5fa098
Zachary_Anderson <zanderson3j@gmail.com>
2019-07-13 20:42:58 -0400

    endTurn tests

--- projects/andezach/dominion/cardtest3.c
+++ projects/andezach/dominion/cardtest3.c
@@ -6,5 +6,25 @@
 #include "rngs.h"
 
 int main() {
-  printf("endTurn Tests\n");
+  int seed = 1000;
+  int numPlayer = 2;
+  int k[10] = {adventurer, minion, ambassador, tribute, mine, remodel, smithy, village, baron, great_hall};
+  struct gameState before, after;
+
+  printf("Turn Should go to Next Player: ");
+  memset(&before, 23, sizeof(struct gameState));
+  initializeGame(numPlayer, k, seed, &before);
+  memcpy(&after, &before, sizeof(struct gameState));
+  endTurn(&after);
+  printf("expected turn: 1, actual: %i\n", after.whoseTurn);
+
+  printf("Last Player Hand is Emptied and New Player is Full: \n");
+  printf("Before: \n");
+  printf("\tLast Player Hand: %i, New Player Hand: %i\n", before.handCount[0], before.handCount[1]);
+  printf("After: \n");
+  printf("\tLast Player Hand: %i, New Player Hand: %i\n", after.handCount[0], after.handCount[1]);
+
+  printf("If Current Player is Last Player, Turn Goes to First Player: ");
+  endTurn(&after);
+  printf("expected turn: 0, actual: %i\n", after.whoseTurn);
 }
