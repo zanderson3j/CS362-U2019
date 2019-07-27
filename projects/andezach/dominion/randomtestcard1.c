85969e4b93364681be3a19011cf19c329fe61b24
Zachary_Anderson <zanderson3j@gmail.com>
2019-07-27 16:45:52 -0400

    display defects

--- projects/andezach/dominion/randomtestcard1.c
+++ projects/andezach/dominion/randomtestcard1.c
@@ -7,28 +7,44 @@
 #include <stdlib.h>
 #include <time.h>
 
+int printFailure(char *message, int currentPlayer, int choice1, struct gameState *before, struct gameState *after, int result) {
+  printf("%s: currentPlayer-%i, choice1-%i, hand-%i %i %i %i %i, coins-%i->%i, result-%i\n",
+            message,
+            currentPlayer,
+            choice1,
+            before->hand[currentPlayer][0],
+            before->hand[currentPlayer][1],
+            before->hand[currentPlayer][2],
+            before->hand[currentPlayer][3],
+            before->hand[currentPlayer][4],
+            before->coins,
+            after->coins,
+            result);
+  return 1;
+}
+
 int testResults(int currentPlayer, int choice1, struct gameState *before, struct gameState *after, int result) {
 
   if (result == 0) {
     int i = 0;
 
-    if (before->numBuys + 1 != after->numBuys) return 1;
+    if (before->numBuys + 1 != after->numBuys) return printFailure("Number of Buys didn't INC", currentPlayer, choice1, before, after, result);
 
     for (i = 0; i < before->handCount[currentPlayer]; ++i) {
       if (before->hand[currentPlayer][i] == baron) break;
     }
 
-    if (i > before->handCount[currentPlayer]) return 1;
+    if (i > before->handCount[currentPlayer]) return printFailure("No Baron in hand", currentPlayer, choice1, before, after, result);
 
     if (choice1 == 1) {
       for (i = 0; i < before->handCount[currentPlayer]; ++i) {
         if (before->hand[currentPlayer][i] == estate) {
           if (before->coins + 4 != after->coins) {
-            return 1;
+            return printFailure("Didn't get 4 coins", currentPlayer, choice1, before, after, result);
           } else if (before->discardCount[currentPlayer] + 1 != after->discardCount[currentPlayer]) {
-            return 1;
+            return printFailure("Didn't discard estate properly", currentPlayer, choice1, before, after, result);
           } else if (before->handCount[currentPlayer] - 1 != after->handCount[currentPlayer]) {
-            return 1;
+            return printFailure("Didn't decrement hand count", currentPlayer, choice1, before, after, result);
           }
           break;
         }
@@ -37,10 +53,10 @@ int testResults(int currentPlayer, int choice1, struct gameState *before, struct
       if (i > before->handCount[currentPlayer]) {
         if (supplyCount(estate, before) > 0) {
           if (before->handCount[0] + 1 != after->handCount[0] || estate != after->hand[0][after->handCount[0] - 1]) {
-            return 1;
+            return printFailure("Didn't pick up estate after not having one to discard", currentPlayer, choice1, before, after, result);
           }
         } else if (before->handCount[0] != after->handCount[0]) {
-          return 1;
+          return printFailure("No estate to pick up, but hand count changed", currentPlayer, choice1, before, after, result);
         }
 
       }
@@ -48,15 +64,15 @@ int testResults(int currentPlayer, int choice1, struct gameState *before, struct
     } else {
       if (supplyCount(estate, before) > 0) {
         if (before->handCount[0] + 1 != after->handCount[0] || estate != after->hand[0][after->handCount[0] - 1]) {
-          return 1;
+          return printFailure("Didn't pick up estate", currentPlayer, choice1, before, after, result);
         }
       } else if (before->handCount[0] != after->handCount[0]) {
-        return 1;
+        return printFailure("No estate to pick up, but hand count changed", currentPlayer, choice1, before, after, result);
       }
     }
 
   } else {
-    return 1;
+    return printFailure("Result not 0", currentPlayer, choice1, before, after, result);
   }
 
   return 0;
@@ -65,7 +81,7 @@ int testResults(int currentPlayer, int choice1, struct gameState *before, struct
 int main() {
 
   const int numOfTests = 2000;
-  int currentPlayer;
+  int currentPlayer = 0;
   int choice1;
   int result;
   int failed = 0;
@@ -75,7 +91,6 @@ int main() {
   srand(time(0));
 
   for (int i = 0; i < numOfTests; ++i) {
-    currentPlayer = rand() % 2; //2 players
     choice1 = rand() % 2;
     memset(&before, 23, sizeof(struct gameState));
     initializeGame(2, k, rand(), &before);
--- projects/andezach/dominion/randomtestcard2.c
+++ projects/andezach/dominion/randomtestcard2.c
@@ -7,38 +7,55 @@
 #include <stdlib.h>
 #include <time.h>
 
+int printFailure(char *message, int currentPlayer, int choice1, int choice2, struct gameState *before, struct gameState *after, int handPos, int result) {
+  printf("%s: currentPlayer-%i, choice1-%i, choice2-%i, hand-%i %i %i %i %i, coins-%i->%i, result-%i\n",
+            message,
+            currentPlayer,
+            choice1,
+            choice2,
+            before->hand[currentPlayer][0],
+            before->hand[currentPlayer][1],
+            before->hand[currentPlayer][2],
+            before->hand[currentPlayer][3],
+            before->hand[currentPlayer][4],
+            before->coins,
+            after->coins,
+            result);
+  return 1;
+}
+
 int testResults(int currentPlayer, int choice1, int choice2, struct gameState *before, struct gameState *after, int handPos, int result) {
   if (result == 0) {
     int i = 0;
 
-    if (before->numActions + 1 != after->numActions) return 1;
+    if (before->numActions + 1 != after->numActions) return printFailure("Didn't increment actions", currentPlayer, choice1, choice2, before, after, handPos, result);
 
     for (i = 0; i < before->handCount[currentPlayer]; ++i) {
       if (before->hand[currentPlayer][i] == minion) break;
     }
 
-    if (i > before->handCount[currentPlayer]) return 1;
+    if (i > before->handCount[currentPlayer]) return printFailure("No minion in hand", currentPlayer, choice1, choice2, before, after, handPos, result);
 
     if (choice1 == choice2 || (choice1 < 0 || choice1 > 1) || (choice2 < 0 || choice2 > 1)) {
-      return 1;
+      return printFailure("Invalid choice", currentPlayer, choice1, choice2, before, after, handPos, result);
     } else if (choice1 == 1) {
-      if (before->coins + 2 != after->coins) return 1;
+      if (before->coins + 2 != after->coins) return printFailure("Didn't add coins", currentPlayer, choice1, choice2, before, after, handPos, result);
       for (i = 0; i < before->numPlayers; ++i) {
-        if (before->coins != after->coins) return 1;
+        if (before->handCount[i] != after->handCount[i] && i != currentPlayer) return printFailure("Hand count changed for other players", currentPlayer, choice1, choice2, before, after, handPos, result);
       }
     } else {
       for (i = 0; i < before->numPlayers; ++i) {
         if (i == currentPlayer || before->handCount[i] > 4) {
-          if (after->handCount[i] != 4) return 1;
+          if (after->handCount[i] != 4) return printFailure("Player didnt draw new hand", currentPlayer, choice1, choice2, before, after, handPos, result);
         } else if (before->handCount[i] != after->handCount[i]) {
-          return 1;
+          return printFailure("Player discarded hand with less than 5 cards", currentPlayer, choice1, choice2, before, after, handPos, result);
         }
       }
-      if (before->coins != after->coins) return 1;
+      if (before->coins != after->coins) return printFailure("Added coins with choice 2", currentPlayer, choice1, choice2, before, after, handPos, result);
     }
 
   } else {
-    return 1;
+    return printFailure("Result not 0", currentPlayer, choice1, choice2, before, after, handPos, result);
   }
   return 0;
 }
@@ -46,7 +63,7 @@ int testResults(int currentPlayer, int choice1, int choice2, struct gameState *b
 int main() {
 
   const int numOfTests = 2000;
-  int currentPlayer;
+  int currentPlayer = 0;
   int choice1;
   int choice2;
   int handPos;
@@ -58,7 +75,6 @@ int main() {
   srand(time(0));
 
   for (int i = 0; i < numOfTests; ++i) {
-    currentPlayer = rand() % 2; //2 players
     choice1 = rand() % 2;
     choice2 = rand() % 2;
     handPos = rand() % 5;
